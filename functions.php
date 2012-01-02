f762599a1a2682de3efc1356411b05df02e62ff8
Arnaud Delante <ad7030@gmail.com>
2012-01-02 22:34:50 +0100

    Initial Release

---
+++ functions.php
@@ -0,0 +1,38 @@
+<?php
+
+function adminBarBottom() {
+    echo '<style type="text/css">
+        body {
+            margin-top: -28px;
+        }
+        #wpadminbar {
+            top: auto !important;
+            bottom: 0;
+        }
+        #wpadminbar .quicklinks .ab-sub-wrapper {
+            bottom: 28px;
+        }
+        #wpadminbar .menupop .ab-sub-wrapper, #wpadminbar .shortlink-input {
+            border-width: 1px 1px 0 1px;
+            -moz-box-shadow:0 -4px 4px rgba(0,0,0,0.2);
+            -webkit-box-shadow:0 -4px 4px rgba(0,0,0,0.2);
+            box-shadow:0 -4px 4px rgba(0,0,0,0.2);
+        }
+        #wpadminbar .quicklinks .menupop ul#wp-admin-bar-wp-logo-default {
+            background-color: #eee;
+        }
+        #wpadminbar .quicklinks .menupop ul#wp-admin-bar-wp-logo-external {
+            background-color: white;
+        }
+    </style>';
+}
+
+// Uncomment if you want it to be done in the Admin Section
+
+// if ( is_admin_bar_showing() ) {
+//     add_action( 'admin_head', 'adminBarBottom' );
+// }
+
+if ( is_admin_bar_showing() ) {
+    add_action( 'wp_head', 'adminBarBottom' );
+}
---
+++ readme.md
@@ -0,0 +1,14 @@
+# [Stick the Wordpress Admin Bar to the bottom](http://www.axioland.me)
+
+This is a simple code which does what it need to do : it put the Wordpress Admin bar to the bottom.
+
+It works with Wordpress 3.3 and the sub-menus work too.
+
+
+## Installation
+
+Grab the code and paste it in the `functions.php` of your theme. Which is in `wp-content/themes/YourTheme/functions.php`
+
+### Optional
+
+Uncomment the little commented part if you want the admin bar to go to the bottom even in the Admin Section.
